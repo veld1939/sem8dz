778d553fe847ead82c6beb81ffbd21e7fd5f9712
veld <1091939@bk.ru>
2023-03-14 07:14:51 +0300

    dz8

---
+++ dz8.1/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int[,] MatrixRandom(int row, int column)
+{
+int[,] matrix = new int[row, column];
+Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+      {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+           {
+           matrix[i, j] = rnd.Next(2, 10);
+           }
+      }
+return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for (int j = 0; j < matrix.GetLength(1); j++)
+       {
+       System.Console.Write(matrix[i, j] + " ");
+       }
+       Console.WriteLine();
+    }
+}
+
+void SortToLower(int[,]matrix )
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k <matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp =matrix [i, k + 1];
+                   matrix [i, k + 1] = matrix[i, k];
+                   matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = MatrixRandom(row, column);
+PrintMatrix(matrix);
+SortToLower(matrix);
+System.Console.WriteLine();
+PrintMatrix(matrix);
---
+++ dz8.2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] count = new int[4, 3];
+ArrayRandom(count);
+PrintArray(count);
+Console.WriteLine();
+SumElements(count);
+
+void SumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < count.GetLength(1); i++)
+    {
+        minRow += count[0, i];
+    }
+    for (int i = 0; i < count.GetLength(0); i++)
+    {
+        for (int j = 0; j < count.GetLength(1); j++) sumRow += count[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
